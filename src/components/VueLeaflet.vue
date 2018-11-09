b44e0a9fa4fbf46a290132db757ad832435544ac
XanxusX <tako30215@hotmail.com>
2018-11-09 23:27:34 +0800

    try to help

--- src/components/VueLeaflet.vue
+++ src/components/VueLeaflet.vue
@@ -1,13 +1,41 @@
 <template>
   <div class="vue-leaflet">
-    <l-map style="width: 100%; height: 600px;" :zoom="zoom" :center="center">
+    <l-map style="width: 100%; height: 600px; z-index: -1000px" :zoom="zoom" :center="center">
       <l-tile-layer :url="url" :attribution="attribution"></l-tile-layer>
       <div v-for="marker in markers">
         <l-marker :lat-lng="[marker.coords.latitude, marker.coords.longitude]">
           <l-popup :content="marker.name"></l-popup>
         </l-marker>
       </div>
-    </l-map> 
+    </l-map>
+    <v-bottom-sheet v-model="sheet">
+      <v-btn
+        slot="activator"
+        color="purple"
+        dark
+      >
+        Click me
+      </v-btn>
+
+      <v-list>
+        <v-subheader>Open in</v-subheader>
+        <v-list-tile
+          v-for="tile in tiles"
+          :key="tile.title"
+          @click="sheet = false"
+        >
+          <v-list-tile-avatar>
+            <v-avatar size="32px" tile>
+              <img
+                :src="`https://cdn.vuetifyjs.com/images/bottom-sheets/${tile.img}`"
+                :alt="tile.title"
+              >
+            </v-avatar>
+          </v-list-tile-avatar>
+          <v-list-tile-title>{{ tile.title }}</v-list-tile-title>
+        </v-list-tile>
+      </v-list>
+    </v-bottom-sheet>
   </div>
 </template>
 
@@ -34,6 +62,14 @@ export default {
       url: 'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
       attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
       markers: model.getMapMarkers(),
+      sheet: false,
+      tiles: [
+        { img: 'keep.png', title: 'Keep' },
+        { img: 'inbox.png', title: 'Inbox' },
+        { img: 'hangouts.png', title: 'Hangouts' },
+        { img: 'messenger.png', title: 'Messenger' },
+        { img: 'google.png', title: 'Google+' }
+      ]
     }
   },
   methods: {
