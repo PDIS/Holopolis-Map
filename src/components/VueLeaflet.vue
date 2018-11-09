eb37920e8427fe33493aee1e446958d6abb38ce3
José manuel Barroso Galindo <theypsilon@gmail.com>
2018-11-09 15:13:05 +0100

    Center on Atocha

--- src/components/VueLeaflet.vue
+++ src/components/VueLeaflet.vue
@@ -23,10 +23,10 @@ export default {
   data () {
     return {
       zoom: 13,
-      center: L.latLng(47.413220, -1.219482),
+      center: L.latLng(40.40613, -3.6903),
       url: 'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
       attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
-      marker: L.latLng(47.413220, -1.219482),
+      marker: L.latLng(40.40613, -3.6903),
       text: 'this is a marker'
     }
   }
