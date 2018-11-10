a93c1bc7695868f0fcc7065f4352dcb9aa749fed
José manuel Barroso Galindo <theypsilon@gmail.com>
2018-11-10 17:16:59 +0100

    Fixing vertical height.

--- src/components/VueLeaflet.vue
+++ src/components/VueLeaflet.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="vue-leaflet">
-    <l-map style="width: 100%; height: 600px; z-index:0" :zoom="zoom" :center="center">
+    <l-map style="width: 100%; height: 100vh; z-index:0" :zoom="zoom" :center="center">
       <l-tile-layer :url="url" :attribution="attribution"></l-tile-layer>
       <div v-for="marker in markers">
         <l-marker :lat-lng="[marker.coords.latitude, marker.coords.longitude]" @click="sheet = true">
