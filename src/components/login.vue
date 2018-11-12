1780fb46c8209cb1cec1af6215646ce7eab153b8
José manuel Barroso Galindo <theypsilon@gmail.com>
2018-11-12 17:09:02 +0100

    Silly login done.

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,12 +1,15 @@
 <template>
   <div>
-    login
+    <v-btn flat color="teal accent-4" class="headline font-weight-light" :to="{name:'VueLeaflet'}">Login</v-btn>
   </div>
 </template>
 
 <script>
+import VueLeaflet from '../components/VueLeaflet'
 export default {
-
+  components: {
+    VueLeaflet
+  },
 }
 </script>
 
