1f5db1adf8f1a613b0aebeb51243c18676d3a41e
XanxusX <tako30215@hotmail.com>
2018-11-14 07:27:48 +0800

    Test

--- src/components/camera.vue
+++ src/components/camera.vue
@@ -1,12 +1,12 @@
 <template>
-  <qrcode-stream :camera="{ facingMode: { ideal: 'environment' }, width: { min: 360, ideal: 680, max: 1920 }, height: { min: 240, ideal: 480, max: 1080 }, }" @decode="onDecode"></qrcode-stream>
+  <qrcode-stream @decode="onDecode"></qrcode-stream>
 </template>
 
 <script>
 export default {
   methods: {
     onDecode (decodedString) {
-      console.log('fuck')
+      console.log('ya')
     }
   }
 }
