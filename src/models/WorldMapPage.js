dded14124f57be6e5fb264a10c53920772150c9c
XanxusX <tako30215@hotmail.com>
2018-11-15 23:29:32 +0800

    Not jpg

--- src/models/WorldMapPage.js
+++ src/models/WorldMapPage.js
@@ -9,7 +9,7 @@ export class WorldMapPage {
 				name: 'Atocha Renfe',
 				conversation_ids: ['92km3zkksd'],
 				coords: {latitude: 40.40613, longitude: -3.6903},
-				logo: 'https://github.com/PDIS/Holopolis-Map/blob/master/src/assets/SPACES.jpg?raw=true',
+				logo: 'https://github.com/PDIS/Holopolis-Map/blob/master/src/assets/SPACES.png?raw=true',
 				icon: L.icon({
 					iconUrl: 'https://github.com/PDIS/Holopolis-Map/blob/master/src/assets/PublicSpaces.gif?raw=true',
 					iconSize: [32, 47.2],
@@ -20,7 +20,7 @@ export class WorldMapPage {
 				name: 'La Ingobernable',
 				conversation_ids: ['3n3dxmbjwt'],
 				coords: {latitude: 40.41163, longitude: -3.69293},
-				logo: 'https://github.com/PDIS/Holopolis-Map/blob/master/src/assets/EQUALITY.jpg?raw=true',
+				logo: 'https://github.com/PDIS/Holopolis-Map/blob/master/src/assets/EQUALITY.png?raw=true',
 				icon: L.icon({
 					iconUrl: 'https://github.com/PDIS/Holopolis-Map/blob/master/src/assets/Equality.gif?raw=true',
 					iconSize: [32, 47.2],
@@ -31,7 +31,7 @@ export class WorldMapPage {
 				name: 'Instituto Nacional de Administración Pública',
 				conversation_ids: ['6amx5ecten', '4rffhe8wa9'],
 				coords: {latitude: 40.40969, longitude: -3.69530},
-				logo: 'https://github.com/PDIS/Holopolis-Map/blob/master/src/assets/HEALTH.jpg?raw=true',
+				logo: 'https://github.com/PDIS/Holopolis-Map/blob/master/src/assets/HEALTH.png?raw=true',
 				icon: L.icon({
 					iconUrl: 'https://github.com/PDIS/Holopolis-Map/blob/master/src/assets/Health.gif?raw=true',
 					iconSize: [32, 47.2],
@@ -42,7 +42,7 @@ export class WorldMapPage {
 				name: 'Real Jardín Botánico de Madrid',
 				conversation_ids: ['4rffhe8wa9'],
 				coords: {latitude: 40.41109, longitude: -3.68978},
-				logo: 'https://github.com/PDIS/Holopolis-Map/blob/master/src/assets/EDUCATION.jpg?raw=true',
+				logo: 'https://github.com/PDIS/Holopolis-Map/blob/master/src/assets/EDUCATION.png?raw=true',
 				icon: L.icon({
 					iconUrl: 'https://github.com/PDIS/Holopolis-Map/blob/master/src/assets/EDUCATION.gif?raw=true',
 					iconSize: [32, 47.2],
