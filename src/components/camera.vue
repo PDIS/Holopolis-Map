ab61c39228e9b6cd3f8778cef488ed4e0d37e4de
José manuel Barroso Galindo <theypsilon@gmail.com>
2018-11-15 11:51:34 +0100

    Camera adjustment 100.

--- src/components/camera.vue
+++ src/components/camera.vue
@@ -31,6 +31,10 @@ export default {
     onInit() {
       const video = document.getElementById('qrcode-stream-container').querySelector('video');
       video.style.height = "100vh";
+      setTimeout(() => {
+        const rect = video.getBoundingClientRect();
+        video.style.height = (+rect.height - 56) + "px";
+      }, 1);
     }
   }
 }
